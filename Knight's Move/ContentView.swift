d7bf5a28d1bda9b5aa6c008e4c70591716a698aa
perevozniuk13 <sashaperevozniuk01@gmail.com>
2025-05-10 10:27:14 +0100

    Initial Commit

---
+++ Knight's Move/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Knight's Move
+//
+//  Created by Oleksandra Perevozniuk on 10/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Knight's Move/Knight_s_MoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Knight_s_MoveApp.swift
+//  Knight's Move
+//
+//  Created by Oleksandra Perevozniuk on 10/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Knight_s_MoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
